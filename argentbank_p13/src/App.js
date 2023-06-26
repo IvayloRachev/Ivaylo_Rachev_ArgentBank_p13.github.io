cbcfad683464b2c4501c81f933b20d7c8beab5df
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-06-26 10:29:56 +0200

    Update App.js

--- argentbank_p13/src/App.js
+++ argentbank_p13/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Footer from './components/Footer';
+import "./styles/style.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
