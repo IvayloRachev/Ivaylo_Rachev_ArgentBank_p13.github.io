17fd85dc5cdb7b536766dec55b178c55f761abd6
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-07-03 12:19:13 +0200

    Add User in App file

--- argentbank_p13/src/App.js
+++ argentbank_p13/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Login from "./pages/Login";
+import User from "./pages/User";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Error />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
       </Routes>
       <Footer />
     </div>
